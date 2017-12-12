0e922956d567ee289245224bebe15ab05ac757fc
khushaalkurswani <lords194@gmail.com>
2017-12-11 17:13:39 -0800

    Update Potentiometer.java

--- src/com/saintsrobotics/corebot/input/sensors/Potentiometer.java
+++ src/com/saintsrobotics/corebot/input/sensors/Potentiometer.java
@@ -1,12 +1,18 @@
 package com.saintsrobotics.corebot.input.sensors;
 
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
-
+/**
+ * This class operates the potentiometer
+ * 
+ */
 public class Potentiometer {
     
     private int pin;
     private AnalogPotentiometer pot;
-    
+    /**
+     * This constructor basically says that the private variable pin is the same as the public variable pin.
+     * @param pin references the pin on the roboRIO on the robot to which the Potentiometer is connected.
+     */
     public Potentiometer(int pin) {
         this.pin = pin;
     }
