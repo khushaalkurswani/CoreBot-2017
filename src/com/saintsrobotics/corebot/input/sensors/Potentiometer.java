338c9fdc00c429d6a3552161753d3619ffe53774
khushaalkurswani <lords194@gmail.com>
2017-12-15 16:58:03 -0800

    Update Potentiometer.java

--- src/com/saintsrobotics/corebot/input/sensors/Potentiometer.java
+++ src/com/saintsrobotics/corebot/input/sensors/Potentiometer.java
@@ -1,24 +1,44 @@
 package com.saintsrobotics.corebot.input.sensors;
 
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
-
+/**
+ * This class operates the potentiometer
+ * 
+ */
 public class Potentiometer {
     
     private int pin;
     private AnalogPotentiometer pot;
-    
+     /**
+      * This constructor basically says that the private variable pin is the same as the public variable pin.
+      * @param pin references the pin on the roboRIO on the robot to which the Potentiometer is connected.
+      */
     public Potentiometer(int pin) {
         this.pin = pin;
     }
-    
+     /**
+      * Initializes the private variable pot as a AnalogPotentiometer().
+      */
     public void init() {
+        /**
+    	 * Controls the Potentiometer in the robot.
+    	 * @param pin (integer) the pin on the roboRIO on the robot to which the Potentiometer is connected.
+    	 * @param fullRange (double) scaling to multiply the fraction by to get a meaningful unit. In this case fullRange is set to 270.
+    	 * @param offset (double) the offset to add to the scaled value for controlling the zero value. In this case the offset is set 0.
+    	 */
         pot = new AnalogPotentiometer(pin, 270, 0);
     }
-    
+     /*
+     * Get the current reading of the potentiometer.
+     * Returns
+     * 			The current reading. 	
+     */
     public double get() {
         return pot.get();
     }
-    
+    /*
+     * Disables the potentiometer
+     */
     public void disable() {
         if (pot != null) {
             pot.free();
